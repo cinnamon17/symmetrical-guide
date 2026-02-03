41a4fd170de6bac817ccac2159f50bc15ccf48ce
cinnamon17 <cinnamon17@lify.win>
2026-02-03 22:35:42 +0100

    fix: leak memory lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,5 +8,14 @@ return {
 	    { "mason-org/mason.nvim", opts = {} },
 	    "neovim/nvim-lspconfig",
 	},
+	config = function()
+	    local lspconfig = require("lspconfig")
+	    lspconfig.jdtls.setup = function() end
+	    lspconfig.lua_ls.setup({})
+	    lspconfig.rust_analyzer.setup({})
+	    lspconfig.phpactor.setup({})
+	    lspconfig.angularls.setup({})
+	    lspconfig.ts_ls.setup({})
+	end
     }
 }
